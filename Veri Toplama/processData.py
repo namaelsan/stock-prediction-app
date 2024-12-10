8a0e1bc933e9cc051cf8b4b48a867e48b31e17f3
namaelsan <x.arybidx@gmail.com>
2024-12-10 16:33:07 +0300

    dataprocessing init

---
+++ Veri Toplama/processData.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import yfinance as yf
+import requests
+
+def getTickers(url):
+    # BIST100 listesindeki şirketlerin Tickerlarını indir
+    return tickers
+
+def getData():
+    # yfinance kullanarak bist100 listesindeki hisselerin son 5 yıldaki hisse değerlerini indir.
+    return data
+
+def mergeData():
+    # getData ile elde edilen hisse değerlerini tek bir MergeData.csv dosyasına yaz
+    return True
+
+if (__name__ == "__main__"):
+    # getData ve mergeData kullanılarak bir csv dosyası oluştur.
+    exit()
