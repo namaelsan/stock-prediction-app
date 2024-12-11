18598882fee774de0e181b30937df62128acea37
namaelsan <x.arybidx@gmail.com>
2024-12-11 23:07:31 +0300

    data update

--- Veri Ön işleme/cleanData.py
+++ Veri Ön işleme/cleanData.py
@@ -11,6 +11,40 @@ def minmaxScale(prices):
     prices_scaled = scaler.fit_transform(prices)
     return prices_scaled
 
+def formatData(csv_files):
+    try:
+        for csv_file in csv_files:
+            with open(f"./Veri Toplama/data/{csv_file}", "r") as fr:
+                # reading line by line
+                lines = fr.readlines()                
+                lines[0] = lines[0].replace("Price", "Date", 1)
+                
+                # pointer for position
+                ptr = 1
+
+
+                # opening in writing mode
+                with open(f"./Veri Toplama/data/{csv_file}", "w") as fw:
+                    for line in lines:
+                        if ptr != 2 and ptr != 3:
+                            fw.write(line)
+                        ptr += 1
+
+        print("Formatted")
+    except:
+        print("Formatting error")
+
+
+def removingUselessData(csv_files):
+    for csv_file in csv_files:
+        dataFrame = pd.read_csv(f"./Veri Toplama/data/{csv_file}")
+        prices = dataFrame['Close'].values.reshape(-1, 1)
+        columnsToKeep = ['Date','Close','Volume']
+        dataFrame = dataFrame[columnsToKeep]
+        if(csv_file == "LMYK.csv"):
+            os.remove(f"./Veri Toplama/data/{csv_file}")
+            continue
+        dataFrame.to_csv(f"./Veri Toplama/data/{csv_file}",index=False)
 
 
 if __name__ == "__main__":
@@ -18,12 +52,7 @@ if __name__ == "__main__":
 
     for file in os.listdir("./Veri Toplama/data/"):
         csv_files.append(file)
-    
-    for csv_file in csv_files:
-        dataFrame = pd.read_csv(f"./Veri Toplama/data/{csv_file}")
-        prices = dataFrame['Close'].values.reshape(-1, 1)
-        prices = prices[2:]
-        prices = minmaxScale(prices)
-        
+    formatData(csv_files)
+    removingUselessData(csv_files)
     
     exit()
